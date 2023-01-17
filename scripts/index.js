59e54dc1169134c70c6ec74cc9d32cdeba76b9d9
Satyam Barik <100867061+Satya2509@users.noreply.github.com>
2023-01-17 22:12:33 +0530

    Add files via upload
    Updated with responsiveness

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let hamburger = document.getElementById("hamburger");
+let menu = document.getElementById("menu");
+
+hamburger.addEventListener('click',function(){
+    menu.classList.toggle('show');
+    
+});
